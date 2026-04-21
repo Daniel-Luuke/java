6a88c8a9abb36399bac463f9bccedc0626cd68d4
daniel luke <daniel@LENOVO-T14-AKCIH6A.localdomain>
2026-04-21 15:40:42 +0300

    new knowledge

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
 //
+const medicineBottle = "fluegone"
+
+console.log(medicineBottle)
+
+packedFood = "fries"
+console.log(packedFood)
+
+const NAME = "Alfred" // "Anastacia 0"
+
+// I am sweet 16 forever, why are you asking my age +=1
+let age = 9
+
+let gradeClass = 4
+
+const admissionNumber = 2512
+
+//2027
+//age + 1
+admissionNumber = 2521
