2c571ddf2aa6f0f9989df005cf6dde792f98eac3
daniel luke <daniel@LENOVO-T14-AKCIH6A.localdomain>
2026-04-21 17:09:09 +0300

    new

--- code.js
+++ code.js
@@ -1,40 +1,37 @@
 //
-const medicineBottle = "fluegone"
+const medicineBottle = "fluegone";
 
-console.log(medicineBottle)
+console.log(medicineBottle);
 
-packedFood = "fries"
-console.log(packedFood)
+packedFood = "fries";
+console.log(packedFood);
 
-const NAME = "Alfred" // "Anastacia 0"
+const NAME = "Alfred"; // "Anastacia 0"
 
 // I am sweet 16 forever, why are you asking my age +=1
-let age = 9
+let age = 9;
 
-let gradeClass = 4
+let gradeClass = 4;
 
-const admissionNumber = 2512
+const admissionNumber = 2512;
 
 //2027
 //age + 1
-admissionNumber = 2521
-
-
+admissionNumber = 2521;
 
 //2026
-let Age = 8
+let Age = 8;
 
 //2027
-age + 1
+age + 1;
 
-console.log(Age)
+console.log(Age);
 
+let oddsArsenalWinningLeague = 2.13;
+let moneyAccount = 20000;
+let payOut = moneyAccount * oddsArsenalWinningLeague;
 
-let oddsArsenalWinningLeague = 2.13
-let moneyAccount  = 20000
-let payOut = moneyAccount * oddsArsenalWinningLeague
+console.log(payOut);
 
-console.log(payOut)
-
-let payoutt = moneyAccount * oddsArsenalWinningLeague
-console.log(payOut)
+let payoutt = moneyAccount * oddsArsenalWinningLeague;
+console.log(payOut);
