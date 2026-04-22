6f0512a791c92acd857a09f9277df9c3ad24ca13
daniel luke <daniel@LENOVO-T14-AKCIH6A.localdomain>
2026-04-22 10:36:34 +0300

    new java

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 // 1. If else statements
 const studentName1 = "Sheila";
-console.log(studentName1);
+const studentGender1 = "Female"
+const studentName2 = "James"
+const studentGender2 = "Male"
+let grade;
+// first itteration
+if(studentGender1 =="Male"){
+    console.log("Accept")
+}else{
+    console.log("Reject")
+}
+
+// second iteration
+if(studentGender1 == "Female"){
+    grade = 87;
+    console.log(grade)
+}else if(studentGender2== "Male"){
+    grade = 90;
+    console.log(grade)
+}
