df345453ecb60558b4ba8d821ef9ecf7837fde09
daniel luke <manthid5@gmail.com>
2026-04-22 10:59:37 +0300

    rename2

--- code.js
+++ code.js
@@ -1,21 +1,21 @@
 // 1. If else statements
 const studentName = "Sheila";
-const studentGender = "Female"
-const studentName1 = "James"
-const studentGender1 = "Male"
+const studentGender = "Female";
+const studentName1 = "James";
+const studentGender1 = "Male";
 let grade;
 // first itteration
-if(studentGender1 =="Male"){
-    console.log("Accept")
-}else{
-    console.log("Reject")
+if (studentGender1 == "Male") {
+  console.log("Accept");
+} else {
+  console.log("Reject");
 }
 
 // second iteration
-if(studentGender == "Female"){
-    grade = 87;
-    console.log(grade)
-}else if(studentGender1 == "Male"){
-    grade = 90;
-    console.log(grade)
+if (studentGender == "Female") {
+  grade = 87;
+  console.log(grade);
+} else if (studentGender1 == "Male") {
+  grade = 90;
+  console.log(grade);
 }
